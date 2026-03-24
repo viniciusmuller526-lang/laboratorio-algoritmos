f2f2d1de72c20642a31a253c298bd46b5deab904
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-24 20:16:49 -0300

    Create Exercicio2.py

---
+++ Semana3/Exercicio2.py
@@ -0,0 +1,12 @@
+# Crux Sacra Sit Mihi Lux
+
+numero1 = float (input ("Digite o primeiro número:"))
+numero2 = float (input ("Digite o segundo núnmero:"))
+
+if numero1 > numero2:
+    
+    print(numero2, numero1)
+    
+else:
+   
+    print(numero1, numero2)
