dc582e777808f7ecb2672e43a5f69f98db8c4033
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-24 21:24:47 -0300

    Create Exercício4.py

---
+++ Semana3/Exercício4.py
@@ -0,0 +1,8 @@
+# Cruz Sacra Sit Mihi Lux
+
+preco_combustivel = float (input ("Insira o preço do combustível: "))
+valor_pagamento = float (input ("Insira o valor do pagamento: "))
+
+litros = valor_pagamento / preco_combustivel
+
+print ("Você conseguiu colocar", litros, "litros")
