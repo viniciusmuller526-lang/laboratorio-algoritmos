4256ee9fd0ffb2ca727992b9ad296cee6309df22
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-24 21:50:49 -0300

    Create Exercício8.py

---
+++ Semana3/Exercício8.py
@@ -0,0 +1,11 @@
+# Cruz Sacra Sit Mihi Lux
+
+valor_compra = float (input ("Digite o valor da sua compra:"))
+
+if valor_compra > 100:
+    
+    valor_compra = valor_compra * 0.9
+    print("Sua compra deu:", valor_compra, "reais")
+    
+else:
+    print("Sua compra deu:", valor_compra, "reais")
