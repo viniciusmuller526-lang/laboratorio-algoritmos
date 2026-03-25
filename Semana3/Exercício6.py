44789102138ddd221a71fd8f3a0a6ab91a54ff57
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-24 21:25:56 -0300

    Create Exercício6.py

---
+++ Semana3/Exercício6.py
@@ -0,0 +1,11 @@
+# Cruz Sacra Sit Mihi Lux
+
+numero = float (input ("Insira um número:"))
+
+if numero >= 0:
+    
+    numero = numero * 2
+    print("Seu número é", numero,)
+    
+else:
+    print("Número inválido")
