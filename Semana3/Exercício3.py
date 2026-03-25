7c77fe95ee7c4686e499026b1b2366bc7cf56786
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-24 21:23:55 -0300

    Create Exercício3.py

---
+++ Semana3/Exercício3.py
@@ -0,0 +1,12 @@
+# Crux Sacra Sit Mihi Lux
+
+idade = int (input ("Insira seu ano de nascimento: "))
+
+idade = 2026 - idade
+
+if idade >= 16:
+    
+    print("Você pode votar")
+    
+else:
+    print("Você não pode votar ainda")
