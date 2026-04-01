983fe4df6bf081fbea4feae1167ee365f82e84bc
Vinicius Müller <viniciusmuller526@gmail.com>
2026-03-31 21:53:58 -0300

    Create Exercício3.py

---
+++ Semana4/Exercício3.py
@@ -0,0 +1,17 @@
+#Crux Sacra Sit Mihi Lux
+
+print("Qual é a distância oficial de uma maratona?")
+print("Alternativas:")
+print("a) 21 km")
+print("b) 42,195 km")
+print("c) 10 km")
+print("d) 5 km")
+
+alternativa = input("Digite a alternativa que considera correta: ")
+
+if alternativa == "b":
+    
+    print("Você acertou!!!")
+    
+else:
+    print("Você errou :(")
