6df52aa1cde93e4200da1ab97ac638c652258bf1
Vinicius Müller <viniciusmuller526@gmail.com>
2026-04-14 21:40:11 -0300

    Create Exercício5.py

---
+++ Semana6/Exercício5.py
@@ -0,0 +1,6 @@
+#Crux Sacra Sit Mihi Lux
+
+oliveiras = int(input("Insira a quantia de oliveiras plantadas: "))
+azeite = oliveiras * 0.05
+
+print("A produção de azeite dessas oliveiras em um ano será de",azeite,"litros")
