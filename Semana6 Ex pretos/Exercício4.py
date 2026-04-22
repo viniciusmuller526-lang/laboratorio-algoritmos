fa3d2a80ff84f38bd02a6fa31f933b4f190c7b7b
Vinicius Müller <viniciusmuller526@gmail.com>
2026-04-22 15:57:52 -0300

    Create Exercício4.py

---
+++ Semana6 Ex pretos/Exercício4.py
@@ -0,0 +1,13 @@
+#Crux Sacra Sit Mihi Lux
+
+idade = int(input("Digite a idade da oliveira: "))
+
+altura = idade * 30
+
+if altura > 500:
+
+    print("Oliveira Adulta")
+
+elif altura <= 500:
+
+    print("Oliveira em crescimento")
