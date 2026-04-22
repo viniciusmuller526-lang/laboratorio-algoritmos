f1d2004c35c553b4189c39c2499bc3f51f965aa2
Vinicius Müller <viniciusmuller526@gmail.com>
2026-04-22 15:58:33 -0300

    Create Exercício6.py

---
+++ Semana6 Ex pretos/Exercício6.py
@@ -0,0 +1,15 @@
+#Crux Sacra Sit Mihi Lux
+
+muda = 1.99
+contador = 1
+
+while contador != 51:
+
+    if contador == 1:
+
+           print(contador,"Muda:", muda * contador)
+           contador += 1
+ 
+    
+    print(contador,"Mudas:", muda * contador)
+    contador += 1 
