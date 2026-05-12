94395a103b45ce507a6432a7b310b9471a5cb9ae
Vinicius Müller <viniciusmuller526@gmail.com>
2026-05-12 20:23:29 -0300

    Create Exercício2.py

---
+++ Semana9/Exercício2.py
@@ -0,0 +1,11 @@
+#Crux Sacra Sit Mihi Lux
+
+somatorio = 0
+
+for contador in range (10):
+    numero = float(input("Digite um número: "))
+    somatorio += numero
+
+media = somatorio / 10
+
+print("A média dos números inseridos é: ", media)
